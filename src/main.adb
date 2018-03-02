230ffc43f325c674016c25026123ececcc0a91a8
Roland4444 <turbopollux@rambler.ru>
2018-03-02 01:31:11 +0000

    simler# Veuillez saisir le message de validation pour vos modifications. Les lignes

--- src/main.adb
+++ src/main.adb
@@ -1,6 +1,10 @@
 with Ada.Text_IO;
 use Ada.Text_IO;
+with loadsets;
 with Interfaces.C; use Interfaces.C;
+with Ada.Calendar;
+use Ada.Calendar;
+with Ada.Integer_Text_IO ; use Ada.Integer_Text_IO ;
 procedure Main is
    function Sys (Arg : Char_Array) return Integer;
    pragma Import(C, Sys, "system");
@@ -9,7 +13,20 @@ procedure Main is
    Last : Natural;
    Ch : Character:='d';
    Available : Boolean:= False;
+   CurrentTime, TargetTime: Time;
+   HoursDelay: Integer;
+   Year    :  Year_Number;
+   Month   :  Month_Number;
+   Day     :  Day_Number;
+   Seconds :  Day_Duration;
+
 begin
+   CurrentTime := Clock;
+   Split(CurrentTime, Year, Month, Day, Seconds);
+   Put(Day);
+   if (Day mod 2 = 0) then Put_Line("Alarm!!!  asparagus need water!!!!");
+      Ret_Val := Sys(To_C("aplay /usr/bin/uss.wav"));
+      end if;
    Put_Line("Welcome to Asparagus Control!");
    Put_Line("Enter command, press <s> - <start> new loop, <b> - break current loop");
    loop
@@ -20,6 +37,12 @@ begin
          if (Ch = 'b') then exit;
             end if;
       end loop;
+      if (Ch='s') then
+         Put_Line("Enter delay (hours)");
+         Get(HoursDelay);
+      --   TargetTime := "+"(Clock,HoursDelay*3600);
+
+         end if;
       --  Get_Line(Str, Last);
       Put_Line("Enter delay from current rime (minutes)");
       Ret_Val := Sys(To_C("aplay vehicle012.wav"));
